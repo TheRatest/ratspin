3664791fe9cd183865642ee0d36962385983b07e
TheRatest <49412643+TheRatest@users.noreply.github.com>
2021-04-08 23:55:02 +0300

    fix

--- ratspin.lua
+++ ratspin.lua
@@ -105,7 +105,7 @@ OtherPage.Button({
         tpservice:Teleport(game.PlaceId, plr)
     end
 })
--- this piece of shit doesn't want to update
+-- AAAAAA
 OtherPage.Button({
     Text = "Update",
     Callback = function()
